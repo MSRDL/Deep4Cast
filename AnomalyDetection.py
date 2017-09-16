f6832432f13c731e43bbcebcbb728979a8909e61
Kenneth Tran <one@kentran.net>
2017-09-15 18:33:39 -0700

    Optimize perf, improve code readability and documentation

--- AnomalyDetection.py
+++ AnomalyDetection.py
@@ -1,36 +1,22 @@
-import math, bisect
-import pandas as pd
-
+import bisect
 import numpy as np
+import pandas as pd
 import matplotlib.pyplot as plt
 
 
-# Given the window size, pre-compute the linear regression coefficients
-def _compute_coefs(w):
-    a11 = w * (w - 1) * (2 * w - 1) / 6
-    a12 = w * (w - 1) / 2
-    a22 = w
-    invariant = 1 / (a11 * a22 - a12 * a12)
-    return a11, a12, a22, invariant
+# Given the window size w, pre-compute X(X^T X)^{-1}X^T - I,
+# in which X = [0   1
+#               1   1
+#               2   1
+#               ...
+#             (w-1) 1]
+def _compute_coef_matrix(w):
+    from numpy import array, arange, ones, linalg, eye
+    X = array([arange(w), ones([w])]).transpose()
+    return X @ linalg.inv(X.transpose() @ X) @ X.transpose() - eye(w)
 
 
-# Given a window, compute the anomaly score as well as the trend
-def _compute_anomaly_score(coefs, values):
-    b1 = b2 = 0
-    for j, y in enumerate(values):
-        b1 += j * y
-        b2 += y
-    slope = (coefs[2] * b1 - coefs[1] * b2) * coefs[3]
-    intercept = (b1 - coefs[0] * slope) / coefs[1]
-
-    residual = 0
-    for j, y in enumerate(values):
-        deviation = slope * j + intercept - y
-        residual += deviation * deviation
-    return math.sqrt(residual / len(values)), slope
-
-
-#TODO ktran: improve the algorithm
+# TODO ktran: improve the algorithm
 def _partition_anomalies(windows, k):
     """
     :param windows: windows, sorted by anomaly score in descending order
@@ -40,52 +26,51 @@ def _partition_anomalies(windows, k):
     diffs = [windows[iw - 1][1] - windows[iw][1]
              for iw in range(1, len(windows))]
     top_jump_positions = sorted(range(len(diffs)), key=lambda i: diffs[i], reverse=True)[0:k]
-    return sorted(top_jump_positions, reverse=True)
+    return sorted(top_jump_positions)
 
 
 def detect_anomalies(data, window_size, sensitivity=5, num_anomalies=None, visualize=True):
     if type(data) != pd.Series:
         raise ValueError('data must be of the pandas Series type')
-    data = data.fillna(0)  #fill NANs with 0 to make the series contiguous
+    data = data.fillna(method='pad')  # fill NANs with 0 to make the series contiguous
 
     w = window_size
-    coefs = _compute_coefs(w)
+    coefs = _compute_coef_matrix(w)
 
-    values = data.values
+    values = data.values.reshape(len(data), 1)
     num_windows = len(values) - w + 1
-    windows = [(ix, _compute_anomaly_score(coefs, values[ix:ix + w])[0])
-               for ix in range(num_windows)]
+    windows = np.hstack(values[ix:ix + window_size] for ix in range(num_windows))
+    residuals = np.linalg.norm(coefs @ windows, axis=0)
 
-    #Sort windows by score in descending order
-    windows.sort(key=lambda item: item[1], reverse=True)
+    windows = [(ix, residuals[ix]) for ix in range(num_windows)]
+    windows.sort(key=lambda item: item[1],
+                 reverse=True)  # sort the windows by their residuals in descending order
 
-    if num_anomalies==0 or sensitivity==0:
+    if num_anomalies == 0 or sensitivity == 0:
         max_anomaly_score = windows[0][1]
         if visualize:
-            print('The maximum anomaly score in the training data is {0:2f}.'.format(max_anomaly_score)
-                  + 'Since you specified no anomaly observed in the historical data, '
-                  + 'the recommended threshold is {0:2f}'.format(max_anomaly_score * 2))
+            print(
+                'The maximum anomaly score in the training data is {0:2f}. '
+                'Since you specified no anomaly observed in the historical data, the recommended threshold is {1:2f}'
+                    .format(max_anomaly_score, max_anomaly_score * 2))
         return [max_anomaly_score * 2]
 
-    #Filter out overlapping windows
+    # Filter out overlapping windows
     num_results = num_anomalies if num_anomalies else num_windows
     iw = 0
-    top_score = windows[iw][1]
-    top_iws = [iw]  #positions of the top windows, after filtering
+    top_iws = [iw]  # positions of the top windows, after filtering
     while len(top_iws) < num_results:
         while iw < num_windows and any(abs(windows[jw][0] - windows[iw][0]) < w for jw in top_iws):
             iw += 1
         if iw < num_windows:
             top_iws.append(iw)
-            score = windows[iw][1]
-            top_score = score
             iw += 1
         else:
             break
     results = [windows[iw] for iw in top_iws]
 
     partition_points = _partition_anomalies(results, sensitivity)
-    thresholds = [(results[jump][1] + results[jump+1][1]) / 2 for jump in partition_points]
+    thresholds = [(results[jump][1] + results[jump + 1][1]) / 2 for jump in partition_points]
 
     timestamps = data.index
     anomalies = []
@@ -104,7 +89,7 @@ def detect_anomalies(data, window_size, sensitivity=5, num_anomalies=None, visua
         data.plot(title='window size: {0}, sensitivity: {1}, #known incidents: {2}'
                   .format(window_size, sensitivity, num_anomalies))
         for anomaly in anomalies.values:
-            plt.axvspan(anomaly[1], anomaly[2], color=plt.cm.jet(0.5 + float(anomaly[0]) / sensitivity / 2), alpha=0.5)
+            plt.axvspan(anomaly[1], anomaly[2], color=plt.cm.jet(0.65 + float(anomaly[0]) / sensitivity / 3), alpha=0.5)
 
     return anomalies, thresholds
 
@@ -112,7 +97,7 @@ def detect_anomalies(data, window_size, sensitivity=5, num_anomalies=None, visua
 # @Lech: please explain this function
 def anomalies_to_series(anomalies, index):
     rows = anomalies.shape[0]
-    series = pd.Series(np.zeros(len(index)), dtype=np.int32)
+    series = pd.Series(np.zeros(len(index)), dtype=np.int)
     series.index = index
     for r in range(rows):
         start = anomalies.loc[r, 'start']
@@ -123,24 +108,22 @@ def anomalies_to_series(anomalies, index):
 
 
 class StreamingAnomalyDetector:
-    def __init__(self, windowSize, thresholds):
+    def __init__(self, window_size, thresholds):
         # This is prototype code and doesn't validate arguments
-        self._w = windowSize;
-        self._thresholds = thresholds;
-        self._buffer = [float('nan')] * windowSize;
-        self._coefs = _compute_coefs(windowSize)
+        self._w = window_size
+        self._thresholds = thresholds
+        self._buffer = np.array([float('nan')] * window_size)
+        self._buffer.shape = (window_size, 1)  # make it vertical
+        self._coef_matrix = _compute_coef_matrix(window_size)
 
     # Update thresholds on demand without restarting the service
     def update_thresholds(self, thresholds):
-        self._thresholds = thresholds;
+        self._thresholds = thresholds
 
     def score(self, value):
-        # REVIEW: in Python, how to shift an array in place without allocating another array?
-        w1 = self._w - 1;
-        self._buffer[0:w1] = self._buffer[-w1:]
-        self._buffer[-1] = value;
-        return _compute_anomaly_score(self._coefs, self._buffer)
+        self._buffer[:-1] = self._buffer[1:]
+        self._buffer[-1] = value
+        return np.linalg.norm(self._coef_matrix @ self._buffer)
 
     def classify(self, value):
-        (score, trend) = self.score(self, value)
-        return bisect.bisect_left(self._thresholds, score)
+        return bisect.bisect_left(self._thresholds, self.score(self, value))
