d7354b552eeb8ee0445c6093dc0181eafe9708ee
Tobias Bischoff <toby@Tobiass-MacBook-Pro.local>
2018-03-24 13:50:50 -0700

    initalized documentation

---
+++ deep_forecast/test.py
@@ -0,0 +1,13 @@
+def public_fn_with_sphinxy_docstring(name, state=None):
+    """This function does something.
+
+    :param name: The name to use. 
+    :type name: str. 
+    :param state: Current state to be in. 
+    :type state: bool. 
+    :returns: int -- the return code. 
+    :raises: AttributeError, KeyError
+
+    """ 
+    return 0
+
