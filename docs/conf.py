59939bb92ef0f28cc59880eedde8863cf4faf6b9
Austin Gross <5520323+austingross@users.noreply.github.com>
2019-05-01 14:37:11 -0700

    fixed broken readthedocs

--- docs/conf.py
+++ docs/conf.py
@@ -2,7 +2,8 @@ import sys
 import os
 
 
-sys.path.insert(0, os.path.abspath('../deep4cast/'))
+sys.path.insert(0, os.path.abspath('../'))
+sys.path.insert(1, os.path.abspath('../deep4cast/'))
 
 extensions = ['sphinx.ext.autodoc', 'nbsphinx']
 source_suffix = '.rst'
