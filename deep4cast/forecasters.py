d6af3943d9601356a8e15b3ceacb5896cb263393
Austin Gross <augross@microsoft.com>
2019-06-28 10:20:57 -0700

    added support for dataparallel; bugfix in predict

--- deep4cast/forecasters.py
+++ deep4cast/forecasters.py
@@ -1,7 +1,8 @@
+import copy
 import time
-import torch
 
 import numpy as np
+import torch
 
 
 class Forecaster():
@@ -9,17 +10,17 @@ class Forecaster():
     from approximate posterior predictive distribution.
 
     Arguments:
-        * model: Deep4cast neural network of class :class:`models`
-        * loss: PyTorch distribution (e.g., Normal)
-        * optimizer: PyTorch optimizer (e.g., Adam)
-        * n_epochs: number of training epochs
-        * device: device used for training (cpu or cuda)
-        * checkpoint_path: path for writing model checkpoints
-        * verbose: switch to toggle verbosity of forecaster during fitting
+        * model (``torch.nn.Module``): Instance of Deep4cast :class:`models`.
+        * loss (``torch.distributions``): Instance of PyTorch `distribution <https://pytorch.org/docs/stable/distributions.html>`_.
+        * optimizer (``torch.optim``): Instance of PyTorch `optimizer <https://pytorch.org/docs/stable/optim.html#torch.optim.Optimizer>`_.
+        * n_epochs (int): Number of training epochs.
+        * device (str): Device used for training (`cpu` or `cuda`).
+        * checkpoint_path (str): File system path for writing model checkpoints.
+        * verbose (bool): Verbosity of forecaster.
     
     """
     def __init__(self,
-                 model: torch.nn.Module,
+                 model,
                  loss=torch.distributions.Normal,
                  optimizer=torch.optim.Adam,
                  n_epochs=1,
@@ -39,14 +40,11 @@ class Forecaster():
         """Fits a model to a given a dataset.
         
         Arguments:
-            * dataloader_train: training data as PyTorch :class:`DataLoader`
-            * dataloader_val: validation data as PyTorch :class:`DataLoader`
-            * eval_model: Flag to switch on model evaluation after every epoch
+            * dataloader_train (``torch.utils.data.DataLoader``): Training data.
+            * dataloader_val (``torch.utils.data.DataLoader``): Validation data.
+            * eval_model (bool): Flag to switch on model evaluation after every epoch.
         
         """
-        if self.verbose:
-            print('Number of model parameters being fitted: {}.'.format(self.model.n_parameters))
-
         # Iterate over training epochs
         start_time = time.time()
         for epoch in range(1, self.n_epochs + 1):
@@ -62,7 +60,14 @@ class Forecaster():
                 self.history['validation'].append(val_loss)
 
     def _train(self, dataloader, epoch, start_time):
-        """Perform training for one epoch."""
+        """Perform training for one epoch.
+
+        Arguments:
+            * dataloader (``torch.utils.data.DataLoader``): Training data.
+            * epoch (int): Current training epoch.
+            * start_time (``time.time``): Clock time of training start.
+            
+        """
         n_trained = 0
         for idx, batch in enumerate(dataloader):
             # Send batch to device
@@ -74,16 +79,16 @@ class Forecaster():
             outputs = self.model(inputs)
             reg = outputs.pop('regularizer')
             loss = -self.loss(**outputs).log_prob(targets).mean() + reg
-            if torch.isnan(loss):
-                raise ValueError('NaN in training loss. NaN budget depleted.')
-            loss.backward()
+            if torch.isnan(loss.mean()):
+                raise ValueError('NaN in training loss.')
+            loss.mean().backward()
             self.optimizer.step()
 
             # Status update for the user
             if self.verbose:
                 n_trained += len(inputs)
                 n_total = len(dataloader.dataset)
-                percentage = 100.0 * idx / len(dataloader)
+                percentage = 100.0 * (idx + 1) / len(dataloader)
                 elapsed = time.time() - start_time
                 remaining = elapsed*((self.n_epochs*n_total)/((epoch-1)*n_total + n_trained) - 1)
                 status = '\rEpoch {}/{} [{}/{} ({:.0f}%)]\t' \
@@ -96,7 +101,7 @@ class Forecaster():
                         n_trained, 
                         n_total, 
                         percentage,
-                        loss.item(),
+                        loss.mean().item(),
                         elapsed // 60,
                         elapsed % 60,
                         remaining // 60, 
@@ -106,10 +111,13 @@ class Forecaster():
                 )            
 
     def _evaluate(self, dataloader, n_samples=10):
-        """Evaluate a model on a dataset.
-        
-        Returns the approximate min negative log likelihood of the model averaged over dataset
+        """Returns the approximate min negative log likelihood of the model 
+        averaged over dataset.
 
+        Arguments:
+            * dataloader (``torch.utils.data.DataLoader``): Evaluation data.
+            * n_samples (int): Number of forecast samples.
+        
         """
         max_llikelihood = [0]*n_samples
         with torch.no_grad():
@@ -129,12 +137,12 @@ class Forecaster():
             
         return -max_llikelihood / len(dataloader.dataset)
 
-    def predict(self, dataloader: torch.utils.data.DataLoader, n_samples=100) -> np.array:
+    def predict(self, dataloader, n_samples=100) -> np.array:
         """Generates predictions.
 
         Arguments:
-            * dataloader: data to make forecasts for as PyTorch :class:`DataLoader`
-            * n_samples: Number of forecast samples
+            * dataloader (``torch.utils.data.DataLoader``): Data to make forecasts.
+            * n_samples (int): Number of forecast samples.
         
         """
         with torch.no_grad():
@@ -147,20 +155,21 @@ class Forecaster():
                     outputs.pop('regularizer')
                     outputs = self.loss(**outputs).sample((1,)).cpu()
                     batch['y'] = outputs[0]
-                    batch = dataloader.dataset.uncompose(batch)
-                    samples.append(outputs)
+                    outputs = copy.deepcopy(batch)
+                    outputs = dataloader.dataset.transform.untransform(outputs)
+                    samples.append(outputs['y'][None, :])
                 samples = np.concatenate(samples, axis=0)
                 predictions.append(samples)
             predictions = np.concatenate(predictions, axis=1)
 
         return predictions
 
-    def embed(self, dataloader: torch.utils.data.DataLoader, n_samples=100) -> np.array:
+    def embed(self, dataloader, n_samples=100) -> np.array:
         """Generate embedding vectors.
 
         Arguments:
-            * dataloader: data to make embedding vectors for as PyTorch :class:`DataLoader`
-            * n_samples: Number of forecast samples
+            * dataloader (``torch.utils.data.DataLoader``): Data to make embedding vectors.
+            * n_samples (int): Number of forecast samples.
         
         """
         with torch.no_grad():
